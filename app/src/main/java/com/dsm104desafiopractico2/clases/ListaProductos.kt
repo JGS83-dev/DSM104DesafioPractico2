95a47bfc9d26c5dde5896f03ec7b7ff613ff0619
jgarcia <js8215001@gmail.com>
2024-04-12 11:46:11 -0600

    config firebase

--- app/src/main/java/com/dsm104desafiopractico2/clases/ListaProductos.kt
+++ app/src/main/java/com/dsm104desafiopractico2/clases/ListaProductos.kt
@@ -2,14 +2,29 @@ package com.dsm104desafiopractico2.clases;
 
 public class ListaProductos {
     var nombre:String? =null
+    var id:String? =null
     var imagen:String? = null
     var precio:Double? = 0.0
+    var tipo:String? = null
 
     constructor(){}
 
     constructor(nombre:String?,precio:Double?){
         this.nombre = nombre
         this.precio = precio
+    }
+
+    constructor(id:String?,nombre:String?,precio:Double?){
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+    }
+
+    constructor(id:String?,nombre:String?,precio:Double?,tipo:String?){
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+        this.tipo = tipo
     }
 
 }
