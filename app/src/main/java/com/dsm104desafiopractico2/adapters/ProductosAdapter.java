ba2e3f2c6215bdb1b5f55602f7bce5df1f3f82dc
jgarcia <js8215001@gmail.com>
2024-04-12 12:52:01 -0600

    Ajuste para agregar a carrito

--- app/src/main/java/com/dsm104desafiopractico2/adapters/ProductosAdapter.java
+++ app/src/main/java/com/dsm104desafiopractico2/adapters/ProductosAdapter.java
@@ -1,9 +1,13 @@
 package com.dsm104desafiopractico2.adapters;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,8 +16,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.dsm104desafiopractico2.R;
 import com.dsm104desafiopractico2.clases.ListaProductos;
-
-import org.w3c.dom.Text;
+import com.dsm104desafiopractico2.ui.dashboard.DashboardFragment;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
@@ -39,6 +44,8 @@ public class ProductosAdapter extends RecyclerView.Adapter<ProductosAdapter.View
         holder.txtNombre.setText(item.getNombre());
         holder.txtPrecio.setText(String.valueOf(item.getPrecio()));
         holder.imagen.setImageResource(R.drawable._01_tacos_de_carnitas__carne_asada_y_al_pastor);
+        holder.producto = item;
+        holder.context = this.context;
     }
 
     @Override
@@ -47,15 +54,30 @@ public class ProductosAdapter extends RecyclerView.Adapter<ProductosAdapter.View
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        public ListaProductos producto = new ListaProductos();
+        public Context context;
         public TextView txtNombre;
         public TextView txtPrecio;
         public ImageView imagen;
+
+        public Button btnAgregar;
 
         public ViewHolder(@NonNull View itemView){
             super(itemView);
             txtNombre = (TextView) itemView.findViewById(R.id.txtNombre);
             txtPrecio = (TextView) itemView.findViewById(R.id.txtPrecio);
             imagen = (ImageView) itemView.findViewById(R.id.imagenProducto);
+            btnAgregar = (Button) itemView.findViewById(R.id.btnAgregar);
+
+            btnAgregar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+                    databaseReference.child("Carrito").child("actual").setValue(producto);
+                    Intent intent = new Intent(context, DashboardFragment.class);
+                    startActivity(context,intent,null);
+                }
+            });
         }
     }
 }
